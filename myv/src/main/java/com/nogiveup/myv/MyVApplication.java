267716ab1e28e4164cbfdac79b8a2cd54ce51257
Jimin <gift040404@naver.com>
2025-07-01 21:28:19 +0900

    [feature] spring boot 초기 설정

---
+++ myv/src/main/java/com/nogiveup/myv/MyVApplication.java
@@ -0,0 +1,13 @@
+package com.nogiveup.myv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVApplication.class, args);
+	}
+
+}
---
+++ myv/src/test/java/com/nogiveup/myv/MyVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nogiveup.myv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
